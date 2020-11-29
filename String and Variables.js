ca9e0736ea574b4ce7c3dd9d4ea1189c6ed2c437
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-11-29 22:58:48 +0600

    Create String and Variables.js

---
+++ String and Variables.js
@@ -0,0 +1,14 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+let errorMessage = 'An error \n' + 
+                    'occured';
+
+alert(errorMessage)
+
+alert(calculationDescription)
+
