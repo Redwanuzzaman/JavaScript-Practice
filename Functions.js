68ccfecc7496d7cc07877bf37c6b845a29c72a36
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-12-01 23:45:28 +0600

    functions

--- Functions.js
+++ Functions.js
@@ -6,3 +6,15 @@ function add(num1, num2){
 let currentResult = add(50, 42);
 
 alert(`The Result is: ${currentResult}`);
+
+
+function add(num1, num2){
+    const result = +num1 + +num2;  // "+" before a variable makes it a number
+    let output = parseInt(num1) + parseInt(num2); // both are same
+    alert(output);
+    return result;
+}
+
+let currentResult = add('50', '42');
+
+alert(`The Result is: ${currentResult}`);
