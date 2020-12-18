570a2d03b7ea582b8e790255dbe8399b469c050d
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-12-19 01:45:29 +0600

    Create Hoisting.js

---
+++ Hoisting.js
@@ -0,0 +1,12 @@
+// JS by default initiates all variables and functions used in a code
+
+console.log(aa);
+let aa;
+
+// Uncaught ReferenceError: aa is not defined
+
+
+console.log(aa);
+var aa;
+
+// undefined
